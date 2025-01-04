a6c6d9a78ccb2d25e4d71f78072e103b83f45c2e
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-01-03 22:52:41 -0300

    2da sesión

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,38 @@ namespace Portafolio.Controllers
         
         public IActionResult Index()
         {
-            return View();
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel { Proyectos = proyectos };
+            return View(modelo);
+        }
+
+        private static List<Proyecto> ObtenerProyectos()
+        {
+            var proyectos = new List<Proyecto>
+            {
+                new() {
+                    Titulo = "CursadaTFC",
+                    Descripcion = "Materiales provistos durante la tutoría del Trabajo Final de Carrera",
+                    ImagenURL = "https://raw.githubusercontent.com/zherar7ordoya/CursadaTFC/refs/heads/main/uai.jpg",
+                    Link = "https://github.com/zherar7ordoya/CursadaTFC"
+                },
+                new ()
+                {
+                    Titulo = "XLS",
+                    Descripcion = "Herramientas para el trabajo en Editora SA",
+                    ImagenURL = "https://raw.githubusercontent.com/zherar7ordoya/XLS/refs/heads/master/vba.png",
+                    Link = "https://github.com/zherar7ordoya/XLS"
+                },
+                new ()
+                {
+                    Titulo = "Xperiments",
+                    Descripcion = "Pequeños experimentos con diferentes lenguajes",
+                    ImagenURL = "https://raw.githubusercontent.com/zherar7ordoya/Xperiments/refs/heads/main/xperiments.jpg",
+                    Link = "https://github.com/zherar7ordoya/Xperiments"
+                }
+            };
+
+            return proyectos;
         }
 
         public IActionResult Privacy()
